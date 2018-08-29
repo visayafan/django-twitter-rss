2ee5654d0a42337a31928e423cf1022ca6e1914b
visayafan <visayafan@163.com>
2018-08-29 22:04:03 +0800

    替换\n为<br> 去掉转推后的换行 精简显示链接

--- twitter/views.py
+++ twitter/views.py
@@ -28,6 +28,41 @@ def format_title(description):
     return title
 
 
+def convert_url(tweet_text):
+    url_tags = tweet_text.find_all(lambda tag: any(attr for attr in tag.attrs if attr == 'data-expanded-url'))
+    if url_tags:
+        for url_tag in url_tags:
+            url = url_tag.get('data-expanded-url')
+            url_tag.replace_with('<a href="{url}">网页链接</a>'.format(url=url))
+    return tweet_text
+
+
+def format_status(url):
+    bst = BeautifulSoup(requests.get(url).content, 'html.parser')
+    # 推特内容
+    tweet_text = bst.find('p', class_='tweet-text')
+    hidden = tweet_text.find('a', class_='u-hidden')
+    if hidden:
+        hidden.extract()
+    tweet_text = convert_url(tweet_text)
+    # 去掉最外层的div，否则若是转推会有换行
+    description = ''.join(map(str, tweet_text.contents))
+    # 引用推特内容
+    quote_author = bst.find('div', class_='QuoteTweet-originalAuthor')
+    quote_text = bst.find('div', class_='QuoteTweet-text')
+    if quote_author:
+        quote_text = convert_url(quote_text)
+        quote_author_username = quote_author.find('span', class_='username').b.text
+        description += ('<div style="border-left: 3px solid gray; padding-left: 1em;">'
+                        '转发@<a href={quote_author_url}>{quote_author_username}</a>：{quote_text}'
+                        '</div>'
+                        ).format(quote_author_url=TWITTER_USER_URL.format(uid=quote_author_username),
+                                 quote_author_username=quote_author_username,
+                                 quote_text=''.join(map(str, quote_text.contents)))
+    description.replace(r'\n', '<br/>')
+    return description
+
+
 def index(request, uid):
     twitter_url = TWITRSS_URL.format(uid=uid)
     b = BeautifulSoup(requests.get(twitter_url).content, 'xml')
@@ -40,25 +75,8 @@ def index(request, uid):
     }
     for item in b.find_all('item'):
         logging.warning(item.link.text)
-        bst = BeautifulSoup(requests.get(item.link.text).content, 'html.parser')
-        # 博主所发推特内容
-        tweet_text = bst.find('p', class_='tweet-text')
-        hidden = tweet_text.find('a', class_='u-hidden')
-        if hidden:
-            hidden.extract()
-        description = str(tweet_text)
-        # 引用推特内容
-        quote_author = bst.find('div', class_='QuoteTweet-originalAuthor')
-        quote_text = bst.find('div', class_='QuoteTweet-text')
-        if quote_author:
-            quote_author_username = quote_author.find('span', class_='username').b.text
-            description += ('<div style="border-left: 3px solid gray; padding-left: 1em;">'
-                            '转发@<a href={quote_author_url}>{quote_author_username}</a>：{quote_text}'
-                            '</div>'
-                            ).format(quote_author_url=TWITTER_USER_URL.format(uid=quote_author_username),
-                                     quote_author_username=quote_author_username,
-                                     quote_text=quote_text)
         item_url = item.link.text
+        description = format_status(item.link.text)
         if item_url.split('/')[3] != uid:
             description = '转推：' + description
         feed_item = {
